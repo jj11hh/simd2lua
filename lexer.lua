6d42e4763167e70d1e19886b6f8a69f6634d08ed
Jiang Yiheng <jj11hh@users.noreply.github.com>
2023-02-21 10:24:29 +0000

    finishing lexer

--- lexer.lua
+++ lexer.lua
@@ -1,8 +1,14 @@
 -- Lexer for simd2lua
 
-Lexer = {}
+local string_find = string.find
+local string_match = string.match
+local string_sub = string.sub
+local table_concat = table.concat
 
-function Lexer.new(input)
+local Lexer = {}
+
+function Lexer.new(input, filename)
+    if filename == nil then filename = "" end
     local self = {
         input = input,
         position = 1,
@@ -10,9 +16,10 @@ function Lexer.new(input)
         backtrace = {},
         line = 1,
         line_pos = 1,
+        filename = filename,
     }
 
-    Lexer.next(self)
+    Lexer.next(self) -- Parse first token
 
     return self
 end
@@ -28,13 +35,13 @@ local function skip_blanks(self)
         local char = string.sub(input, pos, pos)
         local is_blank = char == " " or char == "\t" or char == "\r" or char == "\n"
 
-        if not in_comment and not is_blank then break end
-
         if char == "-" then -- Maybe comment
             if pos+1 <= #input and string.sub(input, pos+1, pos+1) == "-" then
                 in_comment = true
             end
         end
+
+        if not in_comment and not is_blank then break end
 
         if char == "\n" then
             line = line + 1
@@ -43,12 +50,11 @@ local function skip_blanks(self)
         else
             line_pos = line_pos + 1
         end
+
         pos = pos + 1
     end
 
-    self.position = pos
-    self.line = line
-    self.line_pos = line_pos
+    return pos, line, line_pos
 end
 
 local operators = {
@@ -65,7 +71,6 @@ local operators = {
     ["#"] = false,
     ["&"] = false,
     ["|"] = false,
-    ["."] = {".."},
 }
 
 local constants = {
@@ -73,25 +78,24 @@ local constants = {
     ["false"] = false,
 }
 
-local string_find = string.find
-local string_match = string.match
-local string_sub = string.sub
+local function lex_error(self, message)
+    local error_message = {message, " at ", self.filename, ":", self.line, ", character ", tostring(self.line_pos)}
+    error(table_concat(error_message))
+end
 
 -- Copy from https://github.com/GavinHigham/lpil53/blob/master/lexer.lua, with some modification
-local function lex_numeric(self)
-    local input = self.input
-    local init = self.position
+local function lex_numeric(input, init)
+    local i, j, token = string_find(input, '^(0[xX]%x*%.?%x+)', init) --Check for hexidecimal constants
+	local exponent = '^([pP][+-]?%x+)' --Pattern for binary exponent
 
-	local i, j, token = string_find(input, '^(%d*%.?%d+)', init) --Check for numeric constants
-	local exponent = '^([eE][+-]?%d+)' --Pattern for decimal exponent
 	if not i then
-		i, j, token = string_find(input, '^(0[xX]%x*%.?%x+)', init) --Check for hexidecimal constants
+        i, j, token = string_find(input, '^(%d*%.?%d+)', init) --Check for numeric constants
 		if i then
-			exponent = '^([pP][+-]?%x+)' --Pattern for binary exponent
-		else
-			printParserDebug(input, init, lines, "Error: Didn't expect this here.", true)
+            exponent = '^([eE][+-]?%d+)' --Pattern for decimal exponent
 		end
-    else
+    end
+
+    if i then
 		local i2, j2, token2 = string_find(input, exponent, j+1) --Check for trailing exponent
 		if i2 then
 			j, token = j2, token..token2
@@ -102,16 +106,20 @@ end
 
 local function lexer_lex(self)
     local chars = {}
-    skip_blanks(self)
-
+    local pos, line, line_pos = skip_blanks(self)
     local input = self.input
-    local pos = self.position
+    local old_pos = pos
+
+    if pos > #input then
+        self.current = nil
+        return nil, pos, line, line_pos
+    end
 
     local char = string_sub(input, pos, pos)
     local token = char
 
     if string_match(char, "[a-zA-Z_]") then -- Most time here
-        local i, j = string_find(input, "^[0-9a-zA-Z_]+")
+        local i, j = string_find(input, "^[0-9a-zA-Z_]+", pos)
         token = string_sub(input, i, j)
         pos = j+1
         local constant = constants[token]
@@ -121,32 +129,69 @@ local function lexer_lex(self)
         if op ~= nil then -- Operators
             if op == false then -- Single char op
                 token = char
+                pos = pos + 1
             else -- Double char op
                 token = string_sub(input, pos, pos+1) -- No need for boundary check
+                local found = false
                 for i=1,#op do
                     if token == op[i] then
                         token = op[i]
                         pos = pos + #token
+                        found = true
                         break
                     end
                 end
+                if not found then pos = pos + 1 end
             end
         else
-            if string_match(char, "[0-9.]")
+            local i, j, numeric = lex_numeric(input, pos) -- Try get numeric literal
+            if numeric ~= nil then
+                pos = j+1
+                token = tonumber(numeric) -- Parse number out
+            else -- Output current char
+                pos = pos+1
+            end
         end
     end
 
-    self.current = token
-    self.position = pos
-    return token
+    line_pos = line_pos + (pos - old_pos)
+
+    return token, pos, line, line_pos
+end
+
+function Lexer.peek(self)
+    return self.current, self.position, self.line, self.line_pos
 end
 
 function Lexer.next(self)
-    token, new_pos, line, line_pos = lexer_lex(self)
+    local current = self.current
+    local old_pos = self.position
+    local old_line = self.line
+    local old_line_pos = self.line_pos
+
+    local token, new_pos, line, line_pos = lexer_lex(self)
+
     self.current = token
     self.position = new_pos
     self.line = line
     self.line_pos = line_pos
 
-    return token
+    return current, old_pos, old_line, old_line_pos
 end
+
+-- Test
+
+local function lexer_test()
+    local lexer = Lexer.new("1.56 << function main () 2234e2 \n ; -- comment \n   0xFF + +.245 * v0.xyz end ")
+
+    while true do
+        local token = Lexer.next(lexer)
+        print(token)
+
+        if token == nil then return end
+    end
+end
+
+-- lexer_test()
+
+return Lexer
