7529913cb3d51b3c44f153a77ef4f1baed5ed4c9
Jiang Yiheng <jj11hh@users.noreply.github.com>
2023-02-22 03:27:07 +0000

    impl parser code

--- parser.lua
+++ parser.lua
@@ -1,3 +1,5 @@
+local pprint = require("pprint")
+
 -- See also Page 22 in Programming in Lua, 4th Editor
 local binary_operators = {
     { "or" },
@@ -39,7 +41,7 @@ end
 
 local function expect(parser, token)
     if parser.current ~= token then
-        syntax_error(parser, "expecting " .. token)
+        syntax_error(parser, "expecting " .. token .. ", get " .. tostring(parser.current))
     end
 end
 
@@ -51,7 +53,7 @@ end
 
 local function expect_and_take(parser, token)
     expect(parser, token)
-    take(token)
+    take(parser)
 end
 
 local function table_find(t, ele)
@@ -62,6 +64,8 @@ local function table_find(t, ele)
     return nil
 end
 
+local string_match = string.match
+
 local parse_atom
 local parse_postfix
 local parse_expr
@@ -71,57 +75,124 @@ local parse_binary
 local parse_binary_
 local parse_code_block
 local parse_param_decl
+local is_identifier
 
 local if_ends = {"end", "else", "elseif"}
 
-parse_code_block = function(self, ends)
+is_identifier = function(token)
+    return string_match(token, "^[a-zA-Z_][a-zA-Z0-9_]*") ~= nil
+end
+
+local valid_types = {
+    float = true, float2 = true, float3 = true, float4 = true,
+    int = true, int2 = true, int3 = true, int4 = true,
+    -- TODO: Matrix types
+}
+
+local function is_valid_type(token)
+    return valid_types[token] == true
+end
+
+local function parse_type_notation(self)
+    expect_and_take(self, ":")
+    local token = take(self)
+    syntax_assert(self, is_valid_type(token), "expecting type name, get " .. token)
+    return token
+end
+
+local function parse_attributes(self)
+    local attributes = {}
+    if self.current == "<" then
+        take(self)
+        while is_identifier(self.current) do
+            attributes[#attributes+1] = self.current
+            take(self)
+        end
+        expect_and_take(self, ">")
+    end
+    return attributes
+end
+
+local function parse_variable_decl(self)
+    expect_and_take(self, "local")
+    local varname = take(self)
+    syntax_assert(self, is_identifier(varname), "expecting identifier, get " .. varname)
+    local attributes = parse_attributes(self)
+    local vartype = parse_type_notation(self)
+    local init
+    
+    if self.current == "=" then
+        take(self)
+        init = parse_expr(self)
+    end
+
+    self.handler.variable_decl(varname, attributes, vartype, init)
+end
+
+parse_param_decl = function(self)
+    local params = {}
+
+    while true do
+        local token = self.current
+        if not is_identifier(token) then break end
+        take(self)
+        local name = token
+        token = self.current
+        
+        local attributes = parse_attributes(self)
+        local type_notation = parse_type_notation(self)
+
+        -- Type notation
+        local param = {
+            name = name, type = type_notation, attributes = attributes
+        }
+        params[#params+1] = param
+    end
+
+    pprint(params)
+
+    return params
+end
+
+parse_code_block = function(self, ends, ends_with_nil)
     local handler = self.handler
     local cond
     local token
     local ends_with
     if ends == nil then ends = {"end"} end
-    while table_find(ends, self.current) ~= nil do
-        unexpect(self, nil)
+    while table_find(ends, self.current) == nil do
+        if ends_with_nil then
+            if self.current == nil then break end
+        else
+            unexpect(self, nil)
+        end
+
         token = self.current
         if token == "if" then
             take(self)
             cond = parse_expr(self)
             expect_and_take(self, "then")
             handler.process_if(cond)
-            repeat
+            while true do
                 ends_with = parse_code_block(self, if_ends)
                 if ends_with == "end" then 
                     handler.end_if(cond)
                     break 
-                elseif ends_with == "elseif"
+                elseif ends_with == "elseif" then
                     cond = parse_expr(self)
                     handler.process_elseif(cond)
                 elseif ends_with == "else" then
                     handler.process_else()
                 end
             end
-            goto continue
-        end
-
-        if token == "repeat" then
-            take(self)
-            handler.process_repeat()
-            parse_code_block(self)
-            handler.end_repeat()
-            goto continue
-        end
-
-        if token == "while" then
+        elseif token == "while" then
             take(self)
             cond = parse_expr(self)
             expect_and_take(self, "do")
             handler.process_while(cond)
             parse_code_block(self)
             handler.end_while()
-            goto continue
-        end
-
-        if token == "for" then
+        elseif token == "for" then
             take(self)
             local iterator_name = self.current
             syntax_assert(type(iterator_name) ~= "string", "expecting iterator name, get " .. tostring(iterator_name))
@@ -139,32 +210,36 @@ parse_code_block = function(self, ends)
             handler.process_for(iterator_name, begin, ends, step)
             parse_code_block(self)
             handler.end_for()
-            goto continue
-        end
-
-        if token == "function" then
+        elseif token == "function" then
             take(self)
             local function_name = take(self)
             syntax_assert(type(iterator_name) ~= "string", "expecting function name, get " .. tostring(iterator_name))
-            expect_and_take("(")
+
+            expect_and_take(self, "(")
             local params = parse_param_decl(self)
-            expect_and_take(")")
-            handler.process_function_decl(function_name, params)
+            expect_and_take(self, ")")
+
+            local attributes = parse_attributes(self)
+            local return_type = parse_type_notation(self)
+
+            handler.process_function_decl(function_name, params, attributes, return_type)
             parse_code_block(self)
             handler.end_function()
-            goto continue
-        end
-
-        local lvalue = parse_expr(self)
-        if self.current == "=" then -- assignment
+        elseif token == "local" then
+            parse_variable_decl(self)
+        elseif token == "return" then
             take(self)
-            local rvalue = parse_expr(self)
-            handler.assign(lvalue, rvalue)
-        else
-            handler.expr_statement(lvalue)
+            handler.return_val(parse_expr(self))
+        else -- Function call or assignment
+            local lvalue = parse_expr(self)
+            if self.current == "=" then -- assignment
+                take(self)
+                local rvalue = parse_expr(self)
+                handler.assign(lvalue, rvalue)
+            else
+                handler.expr_statement(lvalue)
+            end
         end
-
-        ::continue::
     end
     return take(self)
 end
@@ -186,6 +261,8 @@ parse_binary = function(self, level)
     local a = parse_binary(self, level+1)
     return parse_binary_(self, level, ops, a)
 end
+
+parse_expr = parse_binary
 
 parse_binary_ = function(self, level, ops, a)
     local token = self.current
@@ -226,7 +303,7 @@ parse_atom = function(self)
         expect_and_take(")")
         return expr
     elseif type(token) == "string" then
-        local id = handler.identify(self, token)
+        local id = handler.identifier(token)
         take(self)
         return parse_postfix(self, id)
     elseif type(token) == "number" or type(token) == "boolean" then
@@ -235,6 +312,21 @@ parse_atom = function(self)
     end
 
     syntax_error(self, "expecting atom, get " .. tostring(token))
+end
+
+local function parse_parameters(self)
+    local params = {}
+    while true do
+        params[#params+1] = parse_expr(self)
+
+        if self.current == "," then
+            take(self)
+        else
+            break
+        end
+    end
+
+    return params
 end
 
 parse_postfix = function(self, id)
@@ -249,14 +341,16 @@ parse_postfix = function(self, id)
     elseif token == "[" then
         take(self)
         local index = parse_expr(self)
-        expect_and_take("]")
+        expect_and_take(self, "]")
         return handler.index_access(self, id, index)
     elseif token == "(" then
         take(self)
         local parameters = parse_parameters(self)
-        expect_and_take(")")
+        expect_and_take(self, ")")
         return handler.call(id, parameters)
     end
+
+    return id
 end
 
 -- Tests
@@ -280,4 +374,45 @@ local function test_expr()
     assert(parse_binary(parser) == "(and 1 (~ (+ (- (* 2 2) 3) (* (- 0.002) (not 1))) (~ 255)))")
 end
 
+
+local function test_function()
+    local meta = {}
+    meta.__index = function(t, key)
+        local function handle(...)
+            local output = {"(", key, ", "}
+            local args = {...}
+
+            for i=1,#args do
+                output[#output+1] = tostring(args[i])
+                output[#output+1] = ", "
+            end
+            output[#output] = ")"
+
+            local repr = table.concat(output)
+            print(repr)
+
+            return repr
+        end
+        return handle
+    end
+
+    local handler = {}
+    setmetatable(handler, meta)
+
+    local code = [[
+        function test1(i <inout> :int) :int
+            return i * i
+        end
+
+        function test2() :int
+            local temp :int = 2
+            return test1(3 + 3)
+        end
+    ]]
+
+    local parser = Parser.new(code, handler)
+    parse_code_block(parser, nil, true)
+end
+
 test_expr()
+test_function()
